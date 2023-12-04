b0856a519081093486baff70b42dc1c1977e8dd9
andreamagazzini <andreamagazzini@users.noreply.github.com>
2023-12-04 23:13:33 +0000

    deploy: 29431078ef7bf2666711ed23b1eba94e489bce4a

---
+++ assets/index-j3VduoxA.js
@@ -0,0 +1,1 @@
+import{u as a,r as e,a as o}from"./index-2VYAOvLL.js";function s(){const t=a();e.useEffect(()=>{t(o())})}export{s as Component};
---
+++ assets/index-uC258QxC.js
@@ -0,0 +1,1 @@
+const c=e=>e.replace(/-./g,a=>a[1].toUpperCase());export{c};
