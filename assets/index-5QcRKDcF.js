d34c002244b57bc9fd8d183a713c5c228a8f1ee5
andreamagazzini <andreamagazzini@users.noreply.github.com>
2023-12-04 20:50:44 +0000

    deploy: c3dc8bcc083954d9f12af43a9ac72b4c5a2ee5c8

---
+++ assets/index-5QcRKDcF.js
@@ -0,0 +1,1 @@
+import{u as e,r as o,g as a}from"./index-E65l2Rip.js";function s(){const t=e();o.useEffect(()=>{t(a())})}export{s as Component};
--- assets/index-ySPqwsEY.js
+++
@@ -1,1 +0,0 @@
-import{u as e,r as o,g as a}from"./index-NbMyDvUG.js";function s(){const t=e();o.useEffect(()=>{t(a())})}export{s as Component};
